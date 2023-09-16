1ac5eea30583956fb7fe89cb6e07f2e2d12e2162
Andreas Happe <andreashappe@snikt.net>
2023-09-16 18:17:20 +0200

    make test setup work (unitl running wintermute)

--- testrun.sh
+++ testrun.sh
@@ -14,7 +14,7 @@ vagrant up
 gawk -i inplace '!/^192.168.122./' ~/.ssh/known_hosts
 ./check_ssh_connection.sh
 
-if [$? -eq 0]; then
+if [ $? -eq 0 ]; then
 	echo "virtual machines created!"
 else
 	echo "wasn't able to bring up virtual machines"
@@ -29,6 +29,9 @@ ansible-playbook -i hosts.ini tasks.yaml
 # run tests
 pushd .
 cd $wintermute_dir
+
+# exit for now
+exit 0
 
 # loop over hosts
 for i in $(seq 1 11); do
